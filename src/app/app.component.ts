07d17cee13182ec6b35e8c28bfc101aeed8a9acf
John Rizzo <jrizzo@camcapital.com>
2016-10-03 19:33:48 -0400

    rebased from ng2-admin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
-import { TodoAppComponent } from './todo-app';
+import './app.loader.ts';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { GlobalState } from './global.state';
+import { BaImageLoaderService, BaThemePreloader, BaThemeSpinner } from './theme/services';
+import { layoutPaths } from './theme/theme.constants';
 
+/*
+ * App Component
+ * Top Level Component
+ */
 @Component({
-    moduleId: module.id,
-    selector: 'app-root',
-    templateUrl: 'app.component.html',
-    styleUrls: ['app.component.css'],
-    providers: [TodoAppComponent]
+  selector: 'app',
+  encapsulation: ViewEncapsulation.None,
+  styles: [require('normalize.css'), require('./app.scss')],
+  template: `
+    <main [ngClass]="{'menu-collapsed': isMenuCollapsed}" baThemeRun>
+      <div class="additional-bg"></div>
+      <router-outlet></router-outlet>
+    </main>
+  `
 })
-export class AppComponent {}
+export class App {
+
+  isMenuCollapsed: boolean = false;
+
+  constructor(private _state: GlobalState,
+              private _imageLoader: BaImageLoaderService,
+              private _spinner: BaThemeSpinner) {
+
+    this._loadImages();
+
+    this._state.subscribe('menu.isCollapsed', (isCollapsed) => {
+      this.isMenuCollapsed = isCollapsed;
+    });
+  }
+
+  public ngAfterViewInit(): void {
+    // hide spinner once all loaders are completed
+    BaThemePreloader.load().then((values) => {
+      this._spinner.hide();
+    });
+  }
+
+  private _loadImages(): void {
+    // register some loaders
+    BaThemePreloader.registerLoader(this._imageLoader.load(layoutPaths.images.root + 'sky-bg.jpg'));
+  }
+}
